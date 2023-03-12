405da4944de6bd1011c7047a3733ccbe05b9880c
jeeva <jeevaathecoder@gmail.com>
2023-03-12 12:34:20 +0530

    Binary Search Solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,32 @@
-//https://leetcode.com/problems/richest-customer-wealth/
-//1672. Richest Customer Wealth
-public class Solution {
 
-    public int  maximumWealth(int[][] accounts) {
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
 
-    int ans=Integer.MIN_VALUE;
-        for (int[] account : accounts) {
-            int sum = 0;
-            for (int i : account) {
-                sum = sum + i;
+//Example 1
+//Input: letters = ["c","f","j"], target = "a"
+//Output: "c"
+//Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+//Example 2
+//Input: letters = ["c","f","j"], target = "c"
+//        Output: "f"
+//        Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+    class Solution {
+        public char nextGreatestLetter(char[] letters, char target) {
+
+            int start=0;
+            int end=letters.length-1;
+
+            while(start<=end){
+                int mid = start + (end-start)/2;
+
+                if(target<letters[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
 
             }
-            if (sum > ans) {
-                ans = sum;
-            }
-
+            return letters[start % letters.length];
         }
-    return ans;
-}
-}
+    }
